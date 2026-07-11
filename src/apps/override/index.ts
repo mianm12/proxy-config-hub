4aaaab365220ae90df6e6ecea900a9818340bb54
mianm <m14nlong@gmail.com>
2026-07-12 04:19:18 +0800

    chore: 清理 v2 切换后的冗余文件

--- src/apps/override/index.ts
+++
@@ -1,2 +0,0 @@
-export { runOverrideAdapter } from "./adapter.ts";
-export type { DiagnosticReporter, OverrideContext } from "./adapter.ts";
--- src/apps/rename/index.ts
+++
@@ -1,4 +0,0 @@
-export { runRenameAdapter } from "./adapter.ts";
-export type { DiagnosticReporter, RenameRuntimeData } from "./adapter.ts";
-export { resolveRenameProfile } from "./legacy-arguments.ts";
-export type { RenameArguments } from "./legacy-arguments.ts";
